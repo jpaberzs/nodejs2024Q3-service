552eeefa1d5ba0d127ce739aebf7b3908d0999c5
jpaberzs <janis.paberzs18@gmail.com>
2024-11-10 18:55:40 +0200

    feat: create singelton storage fo data

---
+++ src/database/database.ts
@@ -0,0 +1,9 @@
+import { Album } from 'src/modules/album/interfaces/album';
+import { Artist } from 'src/modules/artists/interfaces/artist';
+import { Track } from 'src/modules/tracks/interfaces/track';
+import { User } from 'src/modules/users/interfaces/user.interface';
+
+export const albumData: Album[] = [];
+export const trackData: Track[] = [];
+export const userData: User[] = [];
+export const artistData: Artist[] = [];
